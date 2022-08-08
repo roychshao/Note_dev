55addd5287c0431fd81184b94be3ab739530180a
roychshao <roych.shao@gmail.com>
2022-08-08 20:00:49 +0800

    [20220808] google strategy use proxy: true

--- routes/auth.js
+++ routes/auth.js
@@ -18,6 +18,7 @@ passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: `${HOST}/auth/google/callback`,
+        proxy: true,
         passReqToCallback: true
     },
     async function (req, res, accessToken, refreshToken, profile, done) {
