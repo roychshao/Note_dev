096c38386957f792a15689add63913b5f17ace0d
roychshao <roych.shao@gmail.com>
2022-08-19 17:26:58 +0800

    test einvoice for project: wallet

--- routes/item.js
+++ routes/item.js
@@ -12,4 +12,57 @@ router.post('/create', auth, insert_item);
 router.post('/mark', auth, mark_item);
 router.post('/delete', auth, delete_item);
 
+router.get('/einvoice', auth, async (req, res) => {
+    
+    const version = 0.5;
+    const cardType = "3J0002";
+    const expTimeStamp = 2147483647;
+    const onlyWinningInv = "N";
+    const uuid = "109703041";
+    const appID = "EINV4202207289482";
+    const url = "https://api.einvoice.nat.gov.tw/PB2CAPIVAN/invServ/InvServ";
+
+    //查詢載具發票表頭
+
+    await axios({
+        method: 'post',
+        url: url,
+        data: qs.stringify({
+            action: "carrierInvChk",
+            appID: appID,
+            cardEncrypt: "620109roy",
+            cardNo: "/23WDYYU",
+            cardType: cardType,
+            endDate: "2022/08/17",
+            expTimeStamp: expTimeStamp,
+            onlyWinningInv: onlyWinningInv,
+            startDate: "2022/07/06",
+            timeStamp: Math.floor(Date.now()/1000)+100, // suggested to add timestamp from 10 to 180
+            uuid: uuid,
+            version: version
+        }),
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+    }).then( async result => {
+        var response = {
+            success: true,
+            message: "取得載具發票表頭資料成功",
+            data: {
+                headers: result.data
+            }
+        };
+        console.log(response);
+        res.status(201).json(response);
+    }).catch(err => {
+        var response = {
+            success: false,
+            message: "取得載具發票表頭失敗 error: " + err.message,
+            data: {}
+        };
+        console.error(response);
+        res.status(400).json(response);
+    })
+})
+
 export default router;
